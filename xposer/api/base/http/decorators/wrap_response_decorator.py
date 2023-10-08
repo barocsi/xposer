6ed44e7e25ace4cbe716342b0db7243ca61d4fce
Aron Barocsi <pfr@skynet>
2023-10-08 16:08:45 +0200

    Added some base classes+

--- xposer/api/base/http/decorators/wrap_response_decorator.py
+++ xposer/api/base/http/decorators/wrap_response_decorator.py
@@ -6,17 +6,21 @@ from fastapi import HTTPException
 from xposer.api.base.http.dto.response_wrapper_dto import ResponseWrapperDTO
 
 
-def ResponseWrapperDecorator(func: Callable):
-    @wraps(func)
-    async def wrapper(*args, **kwargs):
-        meta = {}
-        try:
-            result = await func(*args, **kwargs)
-            meta['total_count'] = 1  # Replace with actual count if available
-            meta['has_more'] = False  # Replace with actual value if available
-            return ResponseWrapperDTO(result="success", data=result, meta=meta)
-        except Exception as e:
-            detail = ResponseWrapperDTO(result="error", exception=str(e), meta=meta).model_dump()
-            raise HTTPException(status_code=500, detail=detail)
+def ResponseWrapperDecorator(ctx):
+    def actual_decorator(func: Callable):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            meta = {}
+            try:
+                result = await func(*args, **kwargs)
+                meta['total_count'] = 1
+                meta['has_more'] = False
+                return ResponseWrapperDTO(result="success", data=result, meta=meta)
+            except Exception as e:
+                ctx.logger.error(f"FastAPI Internal error occurred: {e}")  # Logging the error
+                detail = ResponseWrapperDTO(result="error", exception=str(e), meta=meta).dict()
+                raise HTTPException(status_code=500, detail=detail)
 
-    return wrapper
+        return wrapper
+
+    return actual_decorator
